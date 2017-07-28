7309c597efd219bca032ddb16034ec42ff1675e1
Augustin Bocken <augustin.bocken@hotmail.com>
2017-07-28 14:36:08 +0200

    Adding models

---
+++ MyDigitalKey.Web/Models/Lock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDigitalKey.Web.Models
+{
+    public class Lock
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+    }
+}
