740781cd29374121d52518476dd7b16e3a76c0d3
Duy Nguyen <dnguyen@outlook.be>
2017-07-29 14:26:08 +0200

    add time authorization

--- MyDigitalKey.Services.Contracts/Models/AuthorizationDto.cs
+++ MyDigitalKey.Services.Contracts/Models/AuthorizationDto.cs
@@ -14,7 +14,7 @@ namespace MyDigitalKey.Services.Contracts.Models
         public UserDto User { get; set; }
         public LockDto Lock { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public bool IsActive { get; set; }
     }
 }
